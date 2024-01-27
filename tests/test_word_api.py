17b5de46dc455aa14bd4e8d451e801b258a721bf
roshan4182 <149868016+roshan4182@users.noreply.github.com>
2024-01-27 19:24:52 +0100

    Update test_word_api.py

--- tests/test_word_api.py
+++ tests/test_word_api.py
@@ -1,9 +1,13 @@
 # File: tests/test_word_api.py
 
+import unittest
+from unittest.mock import patch, Mock
 from app.word_api import WordAPI
+from test_main import TestLearnWordApp
 
-def test_get_word():
-    word_api = WordAPI()
-    result = word_api.get()
-    assert "word" in result
-    assert "definition" in result
+class TestLearnWordApp(unittest.TestCase):
+    def test_get_word():
+        word_api = WordAPI()
+        result = word_api.get()
+        assert "word" in result
+        assert "definition" in result
